08ec5bc37f0de9e0cea3dc8902756a0b2374cac4
Oscar Leal <oscarleal234@gmail.com>
2023-03-09 12:09:49 -0600

    got my date and day to show up on header

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let currentDay = $("#currentDay");
+let hour9 = $("#hour-9");
+let hour10 = $("#hour-10");
+let hour11 = $("#hour-11");
+let hour12 = $("#hour-12");
+let hour1 = $("#hour-1");
+let hour2 = $("#hour-2");
+let hour3 = $("#hour-3");
+let hour4 = $("#hour-4");
+let hour5 = $("#hour-5");
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +30,8 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  let today = dayjs().format("dddd, MMM D, YYYY");
+
+  currentDay.text(today);
 });
